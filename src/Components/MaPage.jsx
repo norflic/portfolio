7e8304b986977a2947c8690ee902e51f79495e1a
Derrien Nils <nils.derrien@etu.univ-lyon1.fr>
2025-06-18 10:18:29 +0200

    tests sans images

--- src/Components/MaPage.jsx
+++ src/Components/MaPage.jsx
@@ -1,20 +1,20 @@
 export default function MaPage() {
     return (
         <div id="pres_page" className="flex flex-row max-h-40 bg-blue-200">
-            <span className="w-auto h-auto flex items-center justify-start mb-0">
-                <img className="scale-20" id="head_img" src="/portfolio/head_img.png" alt=""/>
-            </span>
+            {/*<span className="w-auto h-auto flex items-center justify-start mb-0">*/}
+            {/*    <img className="scale-20" id="head_img" src="/portfolio/head_img.png" alt=""/>*/}
+            {/*</span>*/}
             <span className="flex flex-col gap-2 justify-center flex-1">
                 {/*TODO: developper infos*/}
                 <h1>Nils Derrien</h1>
                 <h2>2eme annee d'etude</h2>
                 <h2>spécialité : RA</h2>
-                <button className="flex flex-row flex1" id="btn_cv">
-                    <a href="/portfolio/CV_Nils_DERRIEN.pdf" download="CV de nils Derrien.pdf">
-                        Télecharger mon CV
-                        <img src="/portfolio/icone.svg" alt={""}/>
-                    </a>
-                </button>
+                {/*<button className="flex flex-row flex1" id="btn_cv">*/}
+                {/*    <a href="/portfolio/CV_Nils_DERRIEN.pdf" download="CV de nils Derrien.pdf">*/}
+                {/*        Télecharger mon CV*/}
+                {/*        <img src="/portfolio/icone.svg" alt={""}/>*/}
+                {/*    </a>*/}
+                {/*</button>*/}
             </span>
             <style>
                 {`
