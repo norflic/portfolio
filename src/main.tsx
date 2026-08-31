ff1c11794d97999675f8448514b41cafe5bbd7e5
Derrien Nils <nils.derrien@etu.univ-lyon1.fr>
2026-08-31 11:14:10 +0200

    fix: nouvelle tentative resolution 404 lors du f5

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
 
+const redirectPath = new URLSearchParams(window.location.search).get('redirect');
+if (redirectPath && redirectPath.startsWith('/')) {
+  const appBase = '/portfolio';
+  window.history.replaceState(null, '', `${appBase}${redirectPath}`);
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter basename="portfolio">
+    <BrowserRouter basename="/portfolio">
       <App/>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
